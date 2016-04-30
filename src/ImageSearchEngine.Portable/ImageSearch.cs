76e43553a6504ee051158340e5c2cc2da57a4522
Tsubasa HIRANO <hirano@cuminas.jp>
2016-04-30 22:53:23 +0900

    Add ImageSearch

---
+++ src/ImageSearchEngine.Portable/ImageSearch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Mpga.ImageSearchEngine;
+using System.IO;
+namespace ImageSearchEngine.Portable
+{
+    public class ImageSearch
+    {
+        private readonly string _basePath;
+
+        private readonly ImageInfo[] _info;
+
+        public ImageSearch()
+        {
+        }
+
+        public ImageSearch(ImageInfo[] info)
+        {
+            _info = info;
+        }
+
+        public ImageSearch(string basePath)
+        {
+            _basePath = basePath;
+        }
+
+        public ImageInfo[] LoadFromDb(string dbFileName)
+        {
+        }
+
+        public static string GetDirectory(string path)
+        {
+
+        }
+    }
+}
+
