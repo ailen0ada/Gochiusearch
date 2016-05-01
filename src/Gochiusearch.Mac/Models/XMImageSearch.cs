77778c5081d9b518c2f2e70bb2fcf0e198be59a6
Tsubasa HIRANO <hirano@cuminas.jp>
2016-05-02 01:19:51 +0900

    Fix down sampling algorithm

--- src/Gochiusearch.Mac/Models/XMImageSearch.cs
+++ src/Gochiusearch.Mac/Models/XMImageSearch.cs
@@ -12,25 +12,58 @@ namespace Gochiusearch.Mac
     {
         protected override byte[] GetSmallImageData(string targetFile, int width, int height)
         {
-            using (var imgRep = NSImageRep.ImageRepFromFile(targetFile))
-            using (var cgImage = imgRep.CGImage)
-            using (var cs = CGColorSpace.CreateDeviceRGB())
-            using (var context = new CGBitmapContext(null, width, height, 8, 4 * width, cs, CGImageAlphaInfo.PremultipliedFirst))
+            byte[] bmp32Data;
+            int sourceWidth;
+            int sourceHeight;
+            using (var imgRef = NSImageRep.ImageRepFromFile(targetFile).CGImage)
+            using (var dataProvider = imgRef.DataProvider)
+            using (var data = dataProvider.CopyData())
             {
-                var rect = new CGRect(0, 0, width, height);
-                context.DrawImage(rect, cgImage);
+                var source = data.Bytes;
+                bmp32Data = new byte[data.Length];
+                sourceHeight = (int)imgRef.Height;
+                sourceWidth = (int)imgRef.Width;
+                Marshal.Copy(source, bmp32Data, 0, bmp32Data.Length);
+            }
 
-                using (var newImageRef = context.ToImage())
-                using (var dataProvider = newImageRef.DataProvider)
-                using (var data = dataProvider.CopyData())
+            var result = new byte[width * height * 4];
+            int s = 12; // s*s = サンプリング数
+            int pos = 0;
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
                 {
-                    var source = data.Bytes;
-                    var bytes = new byte[data.Length];
-                    Marshal.Copy(source, bytes, 0, bytes.Length);
+                    int srcX0 = x * sourceWidth / width;
+                    int srcY0 = y * sourceHeight / height;
 
-                    return bytes;
+                    int r = 0;
+                    int g = 0;
+                    int b = 0;
+                    int a = 0;
+
+                    // 縮小した画素に対して縮小元から s * s 画素を取得し
+                    // 平均値を計算
+                    for (int yy = 0; yy < s; yy++)
+                    {
+                        for (int xx = 0; xx < s; xx++)
+                        {
+                            int dx = xx * sourceWidth / width / s;
+                            int dy = yy * sourceHeight / height / s;
+                            int p = ((srcX0 + dx) + (srcY0 + dy) * sourceWidth) * 4;
+                            r += bmp32Data[p];
+                            g += bmp32Data[p + 1];
+                            b += bmp32Data[p + 2];
+                            a += bmp32Data[p + 3];
+                        }
+                    }
+
+                    result[pos++] = (byte)(r / s / s);
+                    result[pos++] = (byte)(g / s / s);
+                    result[pos++] = (byte)(b / s / s);
+                    result[pos++] = (byte)(a / s / s);
                 }
             }
+            return result;
         }
     }
 }
